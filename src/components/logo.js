271dc95cdae3b696089bf17971cd6f813b2d7707
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-05-22 20:36:45 +0530

    :zap: Updating logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
@@ -22,20 +21,19 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
 
   return (
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          {/* <Image src={footPrintImg} width={20} height={20} alt="logo" /> */}
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
             fontWeight="bold"
             ml={3}
           >
-            Takuya Matsuyama
+            Sudeep Deysarker
           </Text>
         </LogoBox>
       </a>
