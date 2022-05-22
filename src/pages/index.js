602cecc0d5b1987568e353ceaee4cd9eaffef701
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-05-22 21:08:21 +0530

    Adding react typed

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import {
   chakra
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Typed from 'react-typed'
 import Paragraph from '../components/paragraph'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
@@ -25,163 +26,178 @@ const ProfileImage = chakra(Image, {
 
 const Home = () => (
   <Layout>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Sudeep Deysarker
-          </Heading>
-          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
-        </Box>
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
-        >
-          <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
+    <Box display={{ md: 'flex' }}>
+      <Box flexGrow={1}>
+        <Heading as="h2" variant="page-title">
+          Sudeep Deysarker
+        </Heading>
+        <p style={{ fontSize: '1rem'}}>
+          I am a{' '}
+          <Typed
+            strings={[
+              'Software Engineer',
+              'Fullstack developer',
+              'Web 3.0 enthusiast'
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            attr="placeholder"
+            loop
           >
-            <ProfileImage
-              src="/images/sudeep-profile.jpg"
-              alt="Profile image"
-              borderRadius="full"
-              width="100%"
-              height="100%"
-            />
-          </Box>
+            <input type="text" style={{ backgroundColor: 'transparent', outline: 'none'}} disabled/>
+          </Typed>
+        </p>
+      </Box>
+      <Box
+        flexShrink={0}
+        mt={{ base: 4, md: 0 }}
+        ml={{ md: 6 }}
+        textAlign="center"
+      >
+        <Box
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          w="100px"
+          h="100px"
+          display="inline-block"
+          borderRadius="full"
+          overflow="hidden"
+        >
+          <ProfileImage
+            src="/images/sudeep-profile.jpg"
+            alt="Profile image"
+            borderRadius="full"
+            width="100%"
+            height="100%"
+          />
         </Box>
       </Box>
+    </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          About
-        </Heading>
-        <Paragraph>
-          Takuya is a freelance and a full-stack developer based in Osaka with a
-          passion for building digital services/stuff he wants. He has a knack
-          for all things launching products, from planning and designing all the
-          way to solving real-life problems with code. When not online, he loves
-          hanging out with his camera. Currently, he is living off of his own
-          product called{' '}
-          <NextLink href="/works/inkdrop" passHref scroll={false}>
-            <Link>Inkdrop</Link>
-          </NextLink>
-          . He publishes content for marketing his products and his YouTube
-          channel called &quot;
-          <NextLink href="https://www.youtube.com/devaslife" passHref>
-            <Link target="_blank">Dev as Life</Link>
-          </NextLink>
-          &quot; has more than 100k subscribers.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
+    <Section delay={0.1}>
+      <Heading as="h3" variant="section-title">
+        About
+      </Heading>
+      <Paragraph>
+        Takuya is a freelance and a full-stack developer based in Osaka with a
+        passion for building digital services/stuff he wants. He has a knack for
+        all things launching products, from planning and designing all the way
+        to solving real-life problems with code. When not online, he loves
+        hanging out with his camera. Currently, he is living off of his own
+        product called{' '}
+        <NextLink href="/works/inkdrop" passHref scroll={false}>
+          <Link>Inkdrop</Link>
+        </NextLink>
+        . He publishes content for marketing his products and his YouTube
+        channel called &quot;
+        <NextLink href="https://www.youtube.com/devaslife" passHref>
+          <Link target="_blank">Dev as Life</Link>
+        </NextLink>
+        &quot; has more than 100k subscribers.
+      </Paragraph>
+      <Box align="center" my={4}>
+        <NextLink href="/works" passHref scroll={false}>
+          <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+            My portfolio
+          </Button>
+        </NextLink>
+      </Box>
+    </Section>
+
+    <Section delay={0.2}>
+      <Heading as="h3" variant="section-title">
+        I ♥
+      </Heading>
+      <Paragraph>
+        Art, Music,{' '}
+        <Link href="https://illust.odoruinu.net/" target="_blank">
+          Drawing
+        </Link>
+        , Playing Drums,{' '}
+        <Link href="https://500px.com/p/craftzdog" target="_blank">
+          Photography
+        </Link>
+        , Leica, Machine Learning
+      </Paragraph>
+    </Section>
+
+    <Section delay={0.3}>
+      <Heading as="h3" variant="section-title">
+        On the web
+      </Heading>
+      <List>
+        <ListItem>
+          <Link href="https://github.com/craftzdog" target="_blank">
+            <Button
+              variant="ghost"
+              colorScheme="teal"
+              leftIcon={<IoLogoGithub />}
+            >
+              @craftzdog
             </Button>
-          </NextLink>
-        </Box>
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Art, Music,{' '}
-          <Link href="https://illust.odoruinu.net/" target="_blank">
-            Drawing
           </Link>
-          , Playing Drums,{' '}
-          <Link href="https://500px.com/p/craftzdog" target="_blank">
-            Photography
-          </Link>
-          , Leica, Machine Learning
-        </Paragraph>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          On the web
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/inkdrop_app" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @inkdrop_app (English)
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @craftzdog (日本語)
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://instagram.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
-
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            href="https://www.youtube.com/devaslife"
-            title="Dev as Life"
-            thumbnail={thumbYouTube}
-          >
-            My YouTube channel (&gt;100k subs)
-          </GridItem>
-          <GridItem
-            href="https://www.inkdrop.app/"
-            title="Inkdrop"
-            thumbnail={thumbInkdrop}
-          >
-            A Markdown note-taking app
-          </GridItem>
-        </SimpleGrid>
-
-        <Box align="center" my={4}>
-          <NextLink href="/posts" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              Popular posts
+        </ListItem>
+        <ListItem>
+          <Link href="https://twitter.com/inkdrop_app" target="_blank">
+            <Button
+              variant="ghost"
+              colorScheme="teal"
+              leftIcon={<IoLogoTwitter />}
+            >
+              @inkdrop_app (English)
             </Button>
-          </NextLink>
-        </Box>
-      </Section>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link href="https://twitter.com/craftzdog" target="_blank">
+            <Button
+              variant="ghost"
+              colorScheme="teal"
+              leftIcon={<IoLogoTwitter />}
+            >
+              @craftzdog (日本語)
+            </Button>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link href="https://instagram.com/craftzdog" target="_blank">
+            <Button
+              variant="ghost"
+              colorScheme="teal"
+              leftIcon={<IoLogoInstagram />}
+            >
+              @craftzdog
+            </Button>
+          </Link>
+        </ListItem>
+      </List>
+
+      <SimpleGrid columns={[1, 2, 2]} gap={6}>
+        <GridItem
+          href="https://www.youtube.com/devaslife"
+          title="Dev as Life"
+          thumbnail={thumbYouTube}
+        >
+          My YouTube channel (&gt;100k subs)
+        </GridItem>
+        <GridItem
+          href="https://www.inkdrop.app/"
+          title="Inkdrop"
+          thumbnail={thumbInkdrop}
+        >
+          A Markdown note-taking app
+        </GridItem>
+      </SimpleGrid>
+
+      <Box align="center" my={4}>
+        <NextLink href="/posts" passHref scroll={false}>
+          <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+            Popular posts
+          </Button>
+        </NextLink>
+      </Box>
+    </Section>
   </Layout>
 )
 
